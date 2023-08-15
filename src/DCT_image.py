e7b2ba5abbf0977ad9fc1b8a50ac1f9618bd9f01
Maxime <kilian.pasquier@hotmail.com>
2023-08-15 19:42:48 +0200

    analyses DCT

--- src/DCT_image.py
+++ src/DCT_image.py
@@ -80,14 +80,14 @@ stego_image_dct = hide_secret(cover_image_dct, secret_image, stego_image_dct)
 #stego_image = idct(stego_image_dct, norm="ortho").astype("uint8")
 stego_image = idct(stego_image_dct, norm="ortho")
 
-'''
+
 #* Transform stego image to uint values
 for i in range(stego_image.shape[0]):
     for j in range(stego_image.shape[1]):
         for k in range(stego_image.shape[2]):
             stego_image[i,j,k] = round(stego_image[i,j,k])
 stego_image = stego_image.astype("uint8")
-'''
+
 
 stego_image_path = os.getcwd() + "/Images/stego_DCT.png"
 stego_image_PIL = Image.fromarray(stego_image, "RGB")
@@ -102,6 +102,6 @@ reconstructed_image = recover_image(stego_image)
 
 # print(reconstructed_image)
 
-reconstructed_image_path = os.getcwd() + "/Images/reconstructed_DCT.png"
+reconstructed_image_path = os.getcwd() + "/Images/reconstructed_DCT_noise.png"
 reconstructed_image_PIL = Image.fromarray(reconstructed_image, "RGB")
 reconstructed_image_PIL.save(reconstructed_image_path)
